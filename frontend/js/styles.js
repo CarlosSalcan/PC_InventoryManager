429d7c6986f062d932567271ad5a756fb5e1620f
Carlos Salcán <cdsalcan131@gmail.com>
2024-05-19 22:30:46 -0500

    Menu correction

--- frontend/js/styles.js
+++ frontend/js/styles.js
@@ -1,16 +1,18 @@
 //-------------------------------> MENU RESPONSIVE
-const showMenu = (toggleId, navId) => {
+const showMenu = (toggleId, navId) =>{
   const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-  toggle.addEventListener('click', () => {
-    // Add show-menu class to nav menu
-    nav.classList.toggle('show-menu')
+  toggle.addEventListener('click', () =>{
+      // Add show-menu class to nav menu
+      nav.classList.toggle('show-menu')
 
-    // Add show-icon to show and hide the menu icon
-    toggle.classList.toggle('show-icon')
+      // Add show-icon to show and hide the menu icon
+      toggle.classList.toggle('show-icon')
   })
 }
+
+showMenu('nav-toggle','nav-menu')
 
 //-------------------------------> VENTANA EMERGENTE
 function mostrarVentanaEmergente(idModal) {
